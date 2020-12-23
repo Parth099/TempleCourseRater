c5d074e6f343794eb6848c6110100ef72b7bb50a
Parth Patel <parthnpatel715@gmail.com>
2020-12-23 15:41:24 -0500

    New Search Filters

--- ProjectApp/filters.py
+++ ProjectApp/filters.py
@@ -9,10 +9,32 @@ class CourseFilter(django_filters.FilterSet):
                                         widget = Input(attrs={'placeholder':'Ex: Chem',}) 
                                         )
 
+    ProgramAPPX = django_filters.CharFilter(
+                                        lookup_expr='icontains', 
+                                        field_name="Program",
+                                        label='Program', 
+                                        widget = Input(attrs={'placeholder':'Ex: Chem',}) 
+                                        )
+
+
     CourseID = django_filters.CharFilter(
                                         label='Course Number', 
                                         widget = Input(attrs={'placeholder':'4 Digit Number', 'class': "form-control",}) 
                                         )
+
+    CourseNumA = django_filters.NumberFilter(
+                                        field_name="CourseNum",
+                                        label='CourseNum Min', 
+                                        widget = Input(attrs={'placeholder':'Starting Range', 'class': "form-control",}),
+                                        lookup_expr="gt",
+                                        )
+    CourseNumB = django_filters.NumberFilter(
+                                        field_name="CourseNum",
+                                        label='CourseNum Max', 
+                                        widget = Input(attrs={'placeholder':'Starting Range', 'class': "form-control",}),
+                                        lookup_expr="lt",
+                                        )
+
     class Meta:
         model = Course
-        fields = ["CourseID", "Program", ]
+        fields = ["CourseID", "Program", "CourseNum"]
